5b09ebebbf7f18b4dd72afbaa74bd040a44b6b3e
johnnyman727 <johnnyman727@gmail.com>
2014-07-31 14:20:59 -0700

    Removing checksum var

--- include/common.h
+++ include/common.h
@@ -1,13 +1,11 @@
 #include <avr/eeprom.h>
 
-extern unsigned short checksum;
-
 // Calculates the checksum of the flash
 void calculate_checksum( unsigned short length);
 
 // Reads the module ID from EEPROM
-uint8_t read_module_id();
+uint8_t read_module_id(void);
 
 // Reads th firmware version from EEPROM
-uint8_t read_firmware_version();
+uint8_t read_firmware_version(void);
 
